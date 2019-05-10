aa459324a9173a8a160d8b9a997dd0714466d02d
Solomon Feldman <solomonrfeldman@gmail.com>
2019-05-10 14:23:11 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,26 @@
 class BankAccount
-
+  attr_reader :name
+  
+  def initialize (name)
+    @name = name
+    @balance = 1000.0
+    @status = "open"
+  end
+  
+  def deposit(cash)
+    @balance += cash
+  end
+  
+  def display_balance
+    @balance
+  end
+  
+  def valid?
+    if @status == open || @balace > 0
+      true
+    else
+      false
+    end
+  end
+  
 end
